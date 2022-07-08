73df980c8cafff7bcd7b4824a3100e3d53e246c2
chabazite <ingallsandrew0@gmail.com>
2022-07-07 20:38:02 -0500

    created garden and plants class as well as addplant, addbed, and addnotes methods

---
+++ src/features/Garden.py
@@ -0,0 +1,68 @@
+class Garden:
+
+    def __init__(self, name, length, width, sqft, type, 
+     sunlight, beds, plant_cost, soil='soil'):
+        """
+        Class for visualizing and storing the user's garden data
+
+        Args:
+            name (string): nickname for the garden
+            length (int): length of the garden
+            width (int): width of the garden
+            sqft (int): square feet of actual planting area
+            type (string): raised beds, greenhouse, etc.
+            sunlight (int): hours of full sunlight the garden recieves
+            beds (int): how many individual beds are there. 
+            plants (list): list of current plant objects in garden
+            plant_cost (int): total cost of garden to buy plants
+            soil (string, optional): is it soil or hydroponics. Defaults to 'soil'.
+
+        """
+        self.name = name
+        self.length = length
+        self.width = width
+        self.sqft = sqft
+        self.type = type
+        self.sunlight = sunlight
+        self.beds = beds
+        self.plant_cost = plant_cost
+        self.soil = soil
+        self.plants = []
+
+    def Addbed(self, n, length, width):
+        """
+        method to add more beds to your garden. These all need to be the same length and width.
+
+        Args:
+            n (int): number of beds to add to your garden
+            length (int): length of individual bed
+            width (int): width of individual bed
+        """
+        self.beds += n
+
+        sqft_beds = n * length * width
+
+        self.sqft += sqft_beds
+
+    def Addplant(self, plant, number):
+        """
+        add a number of a defined plant class to the garden
+
+        Args:
+            plant (_type_): type of plant to add to the garden
+            number (int): number of plants to add to the garden
+        """
+        if plant.sunlight <= self.sunlight:
+            for _ in range(number):
+                self.plants.append(plant)
+            self.sqft -= plant.sqft * number
+            self.plant_cost += plant.cost * number
+        else:
+            answer = input("This plant needs more sunlight than your garden has, do you still wish to plant (y/n): ")
+            if answer == 'y':
+                for _ in range(number):
+                    self.plants.append(plant)
+                self.sqft -= plant.sqft * number
+                self.plant_cost += plant.cost * number
+            else:
+                'o well, lets try a different garden'
---
+++ src/features/Plants.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+
+class Plants:
+
+    def __init__(self, name, variety, difficulty, cost, season,harvest_time, sunlight, soil, plant_date, sqft):
+        """
+        a class for creating and detailing each plant variety that will be used in the garden 
+        Args:
+            name (string): plant general name
+            variety (string): specific plant variety
+            difficulty (string): how hard is it to grow
+            cost (float): cost of the plant
+            season (string): what season does this grow in
+            harvest_time (string): what month is best for harvesting
+            sunlight (int): how many hours of sunlight does it need
+            soil (string): what type of soil
+            plant_date (string): when do you need to plant a seedling
+            sqft (int): how many square feet does it need for planting
+            notes (list): a running notepad for the plant
+        """
+        self.name = name
+        self.variety = variety
+        self.difficulty = difficulty
+        self.cost = cost
+        self.season  = season
+        self.harvest_time = harvest_time
+        self.sunlight = sunlight
+        self.soil = soil
+        self.plant_date = plant_date
+        self.sqft = sqft
+        self.notes = []
+
+    def Addnotes(self, note):
+        """
+        method to add to the note string for this plant 
+        Args:
+            note (string): note to add about the plant
+        """
+        today = datetime.now()
+        datetime_string = today.strftime("%d/%m/%Y %H:%M:%S")
+        note = datetime_string +"--> "+ note
+        self.notes.append(note)
